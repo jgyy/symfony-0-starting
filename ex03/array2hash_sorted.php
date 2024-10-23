b80af3d4358d22c85e3be776a2be49d6651691f8
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-23 21:29:47 +0800

    sorted the algorithm

--- ex03/array2hash_sorted.php
+++ ex03/array2hash_sorted.php
@@ -1,1 +1,19 @@
-array2hash_sorted.php
+<?php
+
+function array2hash_sorted($array) {
+    $result = array();
+    
+    foreach ($array as $item) {
+        if (count($item) >= 2) {
+            $name = $item[0];
+            $age = $item[1];
+            $result[$name] = $age;
+        }
+    }
+    
+    krsort($result);
+    
+    return $result;
+}
+
+?>
