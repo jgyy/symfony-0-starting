43fc799630e1191cf64c0dfc9e18f0efc0d2a301
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-23 21:35:57 +0800

    added capital city function

--- ex04/capital_city_from.php
+++ ex04/capital_city_from.php
@@ -1,1 +1,32 @@
-capital_city_from.php
+<?php
+$states = [
+    'Oregon' => 'OR',
+    'Alabama' => 'AL',
+    'New Jersey' => 'NJ',
+    'Colorado' => 'CO'
+];
+
+$capitals = [
+    'OR' => 'Salem',
+    'AL' => 'Montgomery',
+    'NJ' => 'trenton',
+    'KS' => 'Topeka'
+];
+
+function capital_city_from($state_name) {
+    global $states, $capitals;
+    
+    if (!isset($states[$state_name])) {
+        return "Unknown\n";
+    }
+    
+    $state_code = $states[$state_name];
+    
+    if (!isset($capitals[$state_code])) {
+        return "Unknown\n";
+    }
+    
+    return $capitals[$state_code] . "\n";
+}
+
+?>
