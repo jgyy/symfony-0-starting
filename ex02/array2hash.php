7ee26ec494f335ab8946a66129270fbf17d42e53
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-23 21:07:06 +0800

    added array to hash

--- ex02/array2hash.php
+++ ex02/array2hash.php
@@ -1,1 +1,17 @@
-array2hash.php
+<?php
+
+function array2hash($input_array) {
+    $result = array();
+    
+    foreach ($input_array as $pair) {
+        if (count($pair) >= 2) {
+            $name = $pair[0];
+            $age = $pair[1];
+            $result[$age] = $name;
+        }
+    }
+    
+    return $result;
+}
+
+?>
